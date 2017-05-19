2692c80557816654bdc2f48eaa644f03ea884a2e
ankofler <kofler@oxinia.ch>
2017-05-19 14:20:26 +0200

    java/src/biz/foss-cloud/server/FOSSCloudManager.java

---
+++ java/src/biz/foss-cloud/server/FOSSCloudManager.java
@@ -0,0 +1,24 @@
+/**
+ * FOSS-Cloud Manager
+ * Main web application
+ * (C) 2017 by A. Kofler (info@foss-cloud.biz)
+ * Released under the GNU LESSER GENERAL PUBLIC LICENSE
+ *
+ * The main FOSS-Cloud Manager web application
+ */
+
+package biz.foss-cloud.server;
+
+import org.kubiki.application.server.ApplicationServer;
+import org.kubiki.application.AppLoader;
+
+public class FOSSCloudManager extends ApplicationServer{
+	
+	public FOSSCloudManager(AppLoader apploader){
+		super(apploader);	
+		
+	}
+
+
+
+}
